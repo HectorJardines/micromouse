497cb399dc6e34b3b0814a92b7dac6e535b7e52f
Hector Jardines <jardineshector32@gmail.com>
2025-11-18 22:56:23 -0800

    [feat(l293d.c)] Implement l293d drivers
    If included, commit implements the l293d motor driver user APIs that will be used throughout the project to control motor speed and direction. The driver includes APIs to set direction to forward, reverse, or stop values. Additionally includes an API that allows the user to set speed of the a given motor by specifying a desired pwm duty cycle value.

--- inc/drivers/l293d.h
+++ inc/drivers/l293d.h
@@ -3,13 +3,19 @@
 #include "pwm.h"
 
 typedef enum {
+    dir_stop,
     dir_forward,
-    dir_reverse,
-    dir_turn_right,
-    dir_turn_left,
-    dir_rotate_right,
-    dir_rotate_left
+    dir_reverse
+    // dir_turn_right,
+    // dir_turn_left,
+    // dir_rotate_right,
+    // dir_rotate_left
 } l293d_dir_e;
+
+typedef enum {
+    l293d_motor_left,
+    l293d_motor_right
+} l293d_motor_e;
 
 void l293d_init(void);
 void l293d_set_drive(pwm_channel_e pwm, l293d_dir_e dir, uint16_t duty_cycle_percentage);
--- src/drivers/l293d.c
+++ src/drivers/l293d.c
@@ -1,11 +1,44 @@
 #include "../inc/drivers/l293d.h"
+#include <stdbool.h>
 
+struct cntl_pins {
+    io_e driver_in_1;
+    io_e driver_in_2;
+};
+
+struct cntl_pins driver_input_cnfgs[] = {
+    [l293d_motor_left] = {M1_DRIVER_IN1, M1_DRIVER_IN2},
+    [l293d_motor_right] = {M2_DRIVER_IN1, M2_DRIVER_IN2}
+};
+
+static bool initialized = false;
 void l293d_init(void)
 {
-
+    if (initialized)
+        return;
+    pwm_init();
+    initialized = true;
 }
 
-void l293d_set_drive(pwm_channel_e pwm, l293d_dir_e dir, uint16_t duty_cycle_percentage)
+void l293d_set_dir(pwm_channel_e pwm, l293d_dir_e dir)
 {
+    switch (dir) {
+        case dir_stop:
+            io_set_out(driver_input_cnfgs[pwm].driver_in_1, LOW);
+            io_set_out(driver_input_cnfgs[pwm].driver_in_2, LOW);
+            break;
+        case dir_forward:
+            io_set_out(driver_input_cnfgs[pwm].driver_in_1, LOW);
+            io_set_out(driver_input_cnfgs[pwm].driver_in_2, HIGH);
+            break;
+        case dir_reverse:
+            io_set_out(driver_input_cnfgs[pwm].driver_in_1, HIGH);
+            io_set_out(driver_input_cnfgs[pwm].driver_in_2, LOW);
+            break;
+    }
+}
 
+void l293d_set_speed(pwm_channel_e pwm, uint8_t duty_cycle) {
+    // pwm_channel_enable(pwm, ENABLE);
+    pwm_set_duty_cycle(pwm, duty_cycle);
 }
