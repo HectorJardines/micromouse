be05cb257f0f79dcc5a638346227f47148fc765c
Hector Jardines <jardineshector32@gmail.com>
2026-03-31 22:18:45 -0700

    [feature(log.c)] Implement basic logging functionality
    If included, commit implements a simple logging interface for printf style debugging. As the system grows in complexity we will implement more complex logging functionality that can be used across submodules for different purposes.

---
+++ Core/Inc/common/log.h
@@ -0,0 +1,30 @@
+#ifndef _LOG_H
+#define _LOG_H
+
+#include "uart.h"
+
+#define LOG(fmt, ...) log_message("%s:%d:" fmt "\n", __FILE__, __LINE__, __VA_ARGS__)
+
+#ifndef DISABLE_LOG
+/**
+ * @brief Initializes the logging functionality and required UART driver
+ */
+void log_init(void);
+
+/**
+ * @brief private function called only in the LOG macro wrapper
+ * 
+ * 
+ * 
+ * @param message_fmt[in] C format string used for the log message
+ * @param args[in] arguments that are to be passed to the format string
+ * @return void
+ */
+void log_message(char *fmt, ...);
+
+#else
+#define log_init() ;
+#define log_message(fmt, ...) ;
+#endif
+
+#endif
---
+++ Core/Src/common/log.c
@@ -0,0 +1,21 @@
+#include "log.h"
+#include "uart.h"
+#include "assert_handler.h"
+#include "external/printf/printf.h"
+#include <stdarg.h>
+
+#define LOG(fmt, ...)
+
+static bool initalized = false;
+void log_init(void) {
+    ASSERT(!initalized, ASSERT_APP_LEVEL);
+    usart_init();
+    initalized = true;
+}
+
+void log_message(char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+}
